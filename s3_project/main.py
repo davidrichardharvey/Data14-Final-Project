e85d80ff9ac00524139b879c8c12cd2128d31fb3
Jade <jadearthurs@icloud.com>
2020-08-28 15:27:55 +0100

    finished name changes for cleaning txt

--- s3_project/main.py
+++ s3_project/main.py
@@ -1,12 +1,15 @@
+
+
 from s3_project.classes.cleaning_txt import talent_txt
-from s3_project.classes.academy_class import academy_dataframe
-from s3_project.classes.talent_csv_cleaning import monthly_talent_info
-from s3_project.classes.applicant_info_class import talent_applicant_info
+#from s3_project.classes.academy_class import academy_dataframe
+#from s3_project.classes.talent_csv_cleaning import monthly_talent_info
+#from s3_project.classes.applicant_info_class import talent_applicant_info
+import pandas as pd
+pd.set_option('display.max_rows', talent_txt.to_dataframe().shape[0]+1)
 
-
-print(f"Applicant Info Data Frame: {talent_applicant_info.create_dataframe(talent_applicant_info.clean_files())}")
+#print(f"Applicant Info Data Frame: {talent_applicant_info.create_dataframe(talent_applicant_info.clean_files())}")
 print(f"Talent Text Files Data Frame: {talent_txt.to_dataframe()}")
-print(f"Academy Data Frame: {academy_dataframe.cleaned_df}")
-print(f"Monthly Talent Data Frame: {monthly_talent_info.df_talent_csv}")
+#print(f"Academy Data Frame: {academy_dataframe.cleaned_df}")
+#print(f"Monthly Talent Data Frame: {monthly_talent_info.df_talent_csv}")
 
 
