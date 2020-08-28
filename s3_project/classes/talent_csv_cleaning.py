7f15a5823bb758273982f57959d7bb0aa8fb70cf
Alex <ASam@spartaglobal.com>
2020-08-28 15:06:10 +0100

    'Added split names to use list from config file'

--- s3_project/classes/talent_csv_cleaning.py
+++ s3_project/classes/talent_csv_cleaning.py
@@ -24,11 +24,14 @@ class TalentCsv:
                 Bucket=import_files.bucket_name,
                 Key=index)
             df = pd.read_csv(obj['Body'])
+            print(f"Loaded: {index}")
             df = df.drop(columns='id')
-            df['name'].apply(self.flag_name)
-            df['email'].apply(self.email_valid)
+            for email in df['email']:
+                self.email_valid(email, index)
             df['first_name'] = df['name'].apply(self.splitting_first_names)
             df['last_name'] = df['name'].apply(self.splitting_last_names)
+            for name in df['first_name']:
+                self.flag_name(name, index)
             df['gender'] = df['gender'].apply(self.formatting_gender)
             df['dob'] = df['dob'].apply(self.dob_formatting)
             df['address'] = df['address'].apply(self.format_address)
@@ -36,9 +39,11 @@ class TalentCsv:
             df['degree'] = df['degree'].apply(self.replace_degree)
             df = self.concat_dates(df, 'invited_date', 'month')
             df['invited_by'] = df['invited_by'].apply(self.change_invited_by)
-            df = df[['first_name', 'last_name', 'gender', 'dob', 'email', 'city', 'address', 'postcode', 'phone_number',
+            df = df[['name', 'first_name', 'last_name', 'gender', 'dob', 'email', 'city', 'address', 'postcode', 'phone_number',
                                                                         'uni', 'degree', 'invited_date', 'invited_by']]
+            print(f"Cleaned: {index}")
             self.df_talent_csv = self.df_talent_csv.append(df, ignore_index=True)
+
 
     def cleaning_phone_numbers(self, phone):
         # Takes a phone number as an argument, changes format to fit our requirements
@@ -56,20 +61,29 @@ class TalentCsv:
     def splitting_first_names(self, name):
         # Splits a full name and returns all but the last name
         if type(name) is str:
-            name = name.title()
-            first_name = ' '.join(name.split(' ')[:-1])
+            name_split = name.title().split()
+            common_last_names = find_variable('common_last_names', 'LAST NAMES')
+            first_name = ''
+            for each in name_split:
+                if each.title() in common_last_names:
+                    first_name = ' '.join(name_split[:name_split.index(each.title())])
+                    return first_name
+                else:
+                    first_name = ' '.join(name_split[:-1]).title()
             return first_name
-        else:
-            return name
 
     def splitting_last_names(self, name):
-        # Splits a full name and returns only the last name
         if type(name) is str:
-            name = name.title()
-            last_name = name.split(' ')[-1]
-            return last_name
-        else:
-            return name
+            name_split = name.title().split()
+            common_last_names = find_variable('common_last_names', 'LAST NAMES')
+            common_suffixes = find_variable('common_suffixes', 'LAST NAMES')
+            last_name = ''
+            for each in name_split:
+                if each.title() in common_last_names:
+                    return ' '.join(name_split[name_split.index(each.title()):])
+                else:
+                    last_name = name_split[-2:].title() if name_split[-1] in common_suffixes else name_split[-1]
+            return last_name.replace('.', '')
 
     def formatting_gender(self, gender):
         # This assigns the gender to a single letter M or F
@@ -100,23 +114,25 @@ class TalentCsv:
         df = df.drop(columns=['month', 'new_date'])
         return df
 
-    def flag_name(self, name):
+    def flag_name(self, name, file):
         # This flags the name if there are more than two first names
-        if len(name.split(' ')) > 2:
-            with open(find_variable("talent_csv_issues", "ISSUE FILES"), "a") as ai:
-                ai.writelines(f"{name.title()}\n")
+        if len(name.split(' ')) > 1:
+            with open(find_variable('issues', "ISSUE FILES"), "a") as ai:
+                ai.writelines(f"Filename: {file},   Name: {name.title()},   Issue: Ambiguity in sorting names,    "
+                              f"How Resolved: Ambiguous names put in first name\n")
 
     def format_address(self, address):
         # This formats the addresses to lower case with each word capitalised
         if type(address) is str:
             return address.title()
 
-    def email_valid(self, email):
+    def email_valid(self, email, file):
         # Checks if the email address includes an @ symbol and the fields that don't are written to a text file
         if type(email) is str:
             if '@' not in email:
-                with open("monthly_applicant_emails_edgecases.txt", "a") as ai:
-                    ai.writelines(f"{email}\n")
+                with open(find_variable('issues', "ISSUE FILES"), "a") as ai:
+                    ai.writelines(f"Filename: {file},   Name: {email},   Issue: Invalid email,    "
+                                  f"How Resolved: -\n")
 
     def replace_degree(self, degree):
         # This method checks and converts the degree to the desired format
