71302bd0b399234342d6398474199affbe202baa
Alex <ASam@spartaglobal.com>
2020-09-01 11:16:28 +0100

    'Created joining tables method'

--- s3_project/classes/joining_class.py
+++ s3_project/classes/joining_class.py
@@ -1,27 +1,24 @@
+import pandas as pd
+import numpy as np
 from s3_project.classes.cleaning_txt import talent_txt
 from s3_project.classes.academy_class import academy_dataframe
 from s3_project.classes.talent_csv_cleaning import monthly_talent_info
-#from s3_project.classes.applicant_info_class import talent_applicant_info
-import pandas as pd
-import numpy as np
+from s3_project.classes.applicant_info_class import talent_applicant_info
+
 
 class JoinCleanData:
     def __init__(self):
-        self.talent_txt_df = talent_txt.to_dataframe()
+        self.talent_txt_df = talent_txt.df
         self.monthly_talent_df = monthly_talent_info.df_talent_csv
         self.academy_df = academy_dataframe.cleaned_df
-        self.talent_application_df = ''
+        self.talent_application_df = talent_applicant_info.df_talent_json
+
 
     def first_join(self):
-        talent_txt_df = talent_txt.to_dataframe()
-        monthly_df = monthly_talent_info.df_talent_csv
-        merged_df = pd.merge(monthly_df, talent_txt_df, how='outer', left_on=["first_name", "last_name", 'invited_date']
-                             , right_on=['first_name', 'last_name', 'date'])
-        return merged_df
+        merged_df = pd.merge(self.monthly_talent_df, self.talent_application_df, how='outer', left_on=["first_name",
+                "last_name", 'invited_date'], right_on=['first_name', 'last_name', 'date'])
 
-    def make_table(self, list_of_columns):
-        df = self.join_all[list_of_columns]
-        return df
+        return merged_df
 
     def reassign_FK_column(self, column_name, df):
         # Takes in a df and column_name to reassign foreign key values, returns two data frames which are linked by
@@ -36,10 +33,6 @@ class JoinCleanData:
         #print(unique_values)
         df[column_name] = df[column_name].map(unique_values.get)
         return df, pd.DataFrame(primary_key_table, columns=[column_name])
-
-
-
-
 
 
 clean_data = JoinCleanData()
