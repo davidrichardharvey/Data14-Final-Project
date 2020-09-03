ec267966a8b6f66417512ead6890c550185bca3c
Alex <ASam@spartaglobal.com>
2020-09-03 14:27:56 +0100

    'Tidied up comments'

--- s3_project/classes/joining_class.py
+++ s3_project/classes/joining_class.py
@@ -1,8 +1,8 @@
 from s3_project.functions import all_merges
-# from s3_project.classes.academy_class import Academy
-# from s3_project.classes.talent_csv_cleaning import TalentCsv
-# from s3_project.classes.cleaning_txt import TextFiles
-# from s3_project.classes.applicant_info_class import ApplicantInfoClean
+from s3_project.classes.academy_class import Academy
+from s3_project.classes.talent_csv_cleaning import TalentCsv
+from s3_project.classes.cleaning_txt import TextFiles
+from s3_project.classes.applicant_info_class import ApplicantInfoClean
 from s3_project.Config.config_manager import find_hidden_variable
 from s3_project.Config.config_manager import find_variable
 import pandas as pd
@@ -11,21 +11,20 @@ import ast
 import pyodbc
 from sqlalchemy import create_engine
 
-# academy_dataframe = Academy()
-# monthly_talent_info = TalentCsv()
-# talent_txt = TextFiles()
-# talent_applicant_info = ApplicantInfoClean()
+academy_dataframe = Academy()
+monthly_talent_info = TalentCsv()
+talent_txt = TextFiles()
+talent_applicant_info = ApplicantInfoClean()
 
 
 class JoinCleanData():
     def __init__(self):
-        # self.sparta_day_txt = talent_txt.df
-        # self.monthly_applicant_csv = monthly_talent_info.df_talent_csv
-        # self.academy_scores_csv = academy_dataframe.cleaned_df
-        # self.applicant_info_json = talent_applicant_info.df_talent_json
-        # self.merged_df = all_merges(self.monthly_applicant_csv, self.sparta_day_txt, self.applicant_info_json,
-        #                             self.academy_scores_csv)
-        self.merged_df = pd.read_pickle('merged_dataframe.pkl')
+        self.sparta_day_txt = talent_txt.df
+        self.monthly_applicant_csv = monthly_talent_info.df_talent_csv
+        self.academy_scores_csv = academy_dataframe.cleaned_df
+        self.applicant_info_json = talent_applicant_info.df_talent_json
+        self.merged_df = all_merges(self.monthly_applicant_csv, self.sparta_day_txt, self.applicant_info_json,
+                                    self.academy_scores_csv)
         self.Cities = self.merged_df[['city']]
         self.Course_Interests = self.merged_df[['course_interest']]
         self.Courses = self.merged_df[['course_name', 'course_start_date', 'course_length']]
@@ -61,16 +60,16 @@ class JoinCleanData():
         # This method writes the df to the SQL database
         df.to_sql(sql_table, con=self.engine, index=False, if_exists='append', chunksize=1000)
 
-    # This method gets the ID's from the database for later use
     def get_id(self, sql_id, sql_column_name, table):
+        # This method gets the ID's from the database for later use
         fk_key_dict = {}
         query = f"SELECT {sql_id}, {sql_column_name} FROM {table};"
         for index, value in self._sql_query(query):
             fk_key_dict[value] = index
         return fk_key_dict
 
-    # This method returns unique items from a column which only contains lists
     def values_from_list(self, column_name, df):
+        # This method returns unique items from a column which only contains lists
         df_notnull = df[df[column_name].notna()]
         list_append = []
         for each in df_notnull[column_name]:
@@ -79,8 +78,8 @@ class JoinCleanData():
                     list_append.append(item)
         return list_append
 
-    # This method returns a identifier table for columns that have lists and ensures they are unique in the database
     def identifier_list_tables(self, sql_id, sql_column_name, df_column_name, df, table):
+        # This method returns a identifier table for columns that have lists and ensures they are unique in the database
         unique_values = self.values_from_list(df_column_name, df)
         column_name_dict = self.get_id(sql_id, sql_column_name, table)
         unique_values = pd.Series(unique_values)
@@ -90,8 +89,8 @@ class JoinCleanData():
         id_df = pd.DataFrame(unique_values, columns=[sql_column_name])
         return id_df
 
-    # This method creates a dataframes for a column and ensures they are only unique values
     def identifier_df(self, sql_id, sql_column_name, df_column_name, df, table):
+        # This method creates a dataframes for a column and ensures they are only unique values
         df_notnull = df[df[df_column_name].notna()]
         unique_values = df_notnull.drop_duplicates([df_column_name])
         column_name_dict = self.get_id(sql_id, sql_column_name, table)
@@ -110,8 +109,8 @@ class JoinCleanData():
         id_df = id_df.rename(columns=column_map_dict)
         return id_df
 
-    # This loads all the data to SQL
     def input_tables_to_sql(self):
+        # This loads all the data to SQL
         id_tables = ['Locations', 'Universities', 'Degrees', 'Course_Interests', 'Cities', 'Courses', 'Staff_Roles', 'Strengths', 'Weaknesses']
         df_list = [self.Locations, self.Universities, self.Degrees, self.Course_Interests, self.Cities, self.Courses, self.Staff_Roles, self.Strengths, self.Weaknesses]
         for i in range(len(id_tables)):
@@ -125,21 +124,23 @@ class JoinCleanData():
                 id_df = self.identifier_list_tables(sql_id, sql_column, df_column, df, id_tables[i])
                 print(f"Loading {id_tables[i]} table into database")
                 self.df_to_sql(id_df, id_tables[i])
+                self.sparta.commit()
             else:
                 id_df = self.identifier_df(sql_id, sql_column, df_column, df, id_tables[i])
                 print(f"Loading {id_tables[i]} table into database")
                 self.df_to_sql(id_df, id_tables[i])
-        print('9 tables have loaded.')
+                self.sparta.commit()
+        print('9 tables have successfully loaded')
 
-    # This method reads the keys from the database and maps onto a column
     def reassign_values(self, sql_id, sql_column, df_column, df, table):
+        # This method reads the keys from the database and maps onto a column
         id_dict = self.get_id(sql_id, sql_column, table)
         df[df_column] = df[df_column].map(id_dict)
         df = df.rename(columns={df_column: sql_id})
         return df
 
-    # This methods reassigns every element in the weaknesses column to foreign keys
     def reassign_weaknesses(self, df_column):
+        # This methods reassigns every element in the weaknesses column to foreign keys
         id_dict = self.get_id('keyword_id', 'keyword', 'Weaknesses')
         list_of_keywords = []
         if type(df_column) is not float:
@@ -148,8 +149,8 @@ class JoinCleanData():
                 list_of_keywords.append(value)
         return list_of_keywords
 
-    # This methods reassigns every element in the strengths column to foreign keys
     def reassign_strengths(self, df_column):
+        # This methods reassigns every element in the strengths column to foreign keys
         id_dict = self.get_id('keyword_id', 'keyword', 'Strengths')
         list_of_keywords = []
         if type(df_column) is not float:
@@ -158,23 +159,22 @@ class JoinCleanData():
                 list_of_keywords.append(value)
         return list_of_keywords
 
-    # This method reads the keys from the database and assigns the foreign key value to the appropriate columns
     def apply_reassign(self):
-        print('Reassigning values in dataframe')
-        print('Assigning foreign keys to locations column')
+        # This method reads the keys from the database and assigns the foreign key value to the appropriate columns
+        print('Reassigning values to location column')
         self.merged_df = self.reassign_values('location_id', 'location', 'location', self.merged_df, 'Locations')
-        print('Assigning foreign keys to city column')
+        print('Reassigning values to city column')
         self.merged_df = self.reassign_values('city_id', 'city', 'city', self.merged_df, 'Cities')
-        print('Assigning foreign keys to course interest column')
+        print('Reassigning values to course interest column')
         self.merged_df = self.reassign_values('course_type_id', 'course_type', 'course_interest', self.merged_df, 'Course_Interests')
-        print('Assigning foreign keys to courses column')
+        print('Reassigning values to courses column')
         self.merged_df = self.reassign_values('course_id', 'course_name', 'course_name', self.merged_df, 'Courses')
-        print('Assigning foreign keys to degree column')
+        print('Reassigning values to degree column')
         self.merged_df = self.reassign_values('degree_id', 'degree', 'degree', self.merged_df, 'Degrees')
-        print('Assigning foreign keys to uni column')
+        print('Reassigning values to uni column')
         self.merged_df = self.reassign_values('uni_id', 'uni', 'uni', self.merged_df, 'Universities')
-        print('Assigning foreign keys to strengths column')
+        print('Reassigning values to strengths column')
         self.merged_df['strengths'] = self.merged_df['strengths'].apply(self.reassign_strengths)
-        print('Assigning foreign keys to weaknesses column')
+        print('Reassigning values to weaknesses column')
         self.merged_df['weaknesses'] = self.merged_df['weaknesses'].apply(self.reassign_weaknesses)
         print('Reassignment completed')
