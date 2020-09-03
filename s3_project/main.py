8d205cf702526e7c114594888e10bd7b1411806a
Jade <jadearthurs@icloud.com>
2020-09-03 10:38:06 +0100

    finding files

--- s3_project/main.py
+++ s3_project/main.py
@@ -1,13 +1,13 @@
-# from s3_project.classes.cleaning_txt import talent_txt
+from s3_project.classes.cleaning_txt import talent_txt
 from s3_project.classes.academy_class import academy_dataframe
-# from s3_project.classes.talent_csv_cleaning import monthly_talent_info
-# from s3_project.classes.applicant_info_class import talent_applicant_info
+from s3_project.classes.talent_csv_cleaning import monthly_talent_info
+from s3_project.classes.applicant_info_class import talent_applicant_info
 #
 #
-# print(f"Applicant Info Data Frame: {talent_applicant_info.df_talent_json}")
-# print(f"Talent Text Files Data Frame: {talent_txt.df}")
+print(f"Applicant Info Data Frame: {talent_applicant_info.df_talent_json}")
+print(f"Talent Text Files Data Frame: {talent_txt.df}")
 print(f"Academy Data Frame: {academy_dataframe.cleaned_df}")
-# print(f"Monthly Talent Data Frame: {monthly_talent_info.df_talent_csv}")
+print(f"Monthly Talent Data Frame: {monthly_talent_info.df_talent_csv}")
 
 
 # from s3_project.classes.create_database import new
